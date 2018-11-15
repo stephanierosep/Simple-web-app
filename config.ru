aa5e33ac0de57cbb0db967e20420a6e9c06816a3
Stephanie Parker <stephanieparker@pc-129-99.customer.cityuniversity.generation.lan>
2018-11-15 02:37:30 +0000

    WIP sessions currently not storing info

---
+++ config.ru
@@ -0,0 +1,2 @@
+require './shopping_list'
+run Shopping_list
--- shopping_list.rb
+++ shopping_list.rb
@@ -1,22 +1,40 @@
 require 'sinatra'
-enable :sessions
 
-get '/' do
-  p "I am at /"
-  erb(:index)
-end
+# class Shopping_list < Sinatra::Base
+#   use Rack::Session::Pool
 
-post '/list' do
-  p "I am at list"
-  p params
-  @item = params[:item]
-  redirect to('/my_list')
-end
+  # configure do
+  enable :sessions
+  # set :session_secret, "My session secret"
+  # end
 
-get '/my_list' do
-  p "I am at my list"
-   p @item
-  # p params
-  # @item = params[:item]
-  erb(:updated_list)
-end
+  get '/' do
+    erb(:index)
+  end
+
+  post '/list' do
+    p params
+    session[:item] = params[:item]
+    p "line 18"
+    p session[:item]
+    p "item is " << session[:item]
+    p "line 20"
+    redirect to('/my_list')
+  end
+
+  get '/my_list' do
+    p "value = " << session[:item].inspect
+    @item = session[:item]
+    p "line 25"
+    p session[:item]
+    p "line 27"
+    erb(:updated_list)
+  end
+
+  get '/checkout' do
+    erb(:checkout)
+  end
+
+  get '/homepage' do
+    erb(:homepage)
+  end
